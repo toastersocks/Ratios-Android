342de64aaa0872387176cae2f445dd8742c03d20
James Pamplona <pamplonajames@gmail.com>
2018-03-01 16:34:33 -0800

    Initial commit

---
+++ app/src/main/java/com/toastersocks/ratios/RatiosActivity.kt
@@ -0,0 +1,12 @@
+package com.toastersocks.ratios
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RatiosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ratios)
+    }
+}
